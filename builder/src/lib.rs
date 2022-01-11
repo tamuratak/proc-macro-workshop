e74074bc087783b32e743a9427f6143aa111c1bb
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-01-11 18:12:57 +0900

    Add examples/

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
 
-    unimplemented!()
+    let expanded = quote! {};
+    TokenStream::from(expanded)
 }
